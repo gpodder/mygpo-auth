a1cb27fdf9065b2d48e442b0cac1840392fc83be
Stefan Kögl <stefan@skoegl.net>
2015-04-06 14:57:53 +0200

    Fix PEP8 violations

--- mygpoauth/settings.py
+++ mygpoauth/settings.py
@@ -1,3 +1,6 @@
+import os
+import dj_database_url
+
 """
 Django settings for mygpoauth project.
 
@@ -11,8 +14,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -74,7 +75,6 @@ WSGI_APPLICATION = 'mygpoauth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(),
 }
