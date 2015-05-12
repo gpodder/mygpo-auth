b9c6782a1c06b566572e6255c7b3fd4756a76a34
Stefan Kögl <stefan@skoegl.net>
2015-05-12 19:39:04 +0200

    Redirect / to read-the-docs

--- mygpoauth/urls.py
+++ mygpoauth/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from mygpoauth import oauth2
 
@@ -9,6 +10,8 @@ urlpatterns = [
     # url(r'^$', 'mygpoauth.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='http://mygpo-auth.rtfd.org/'),
+        name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth2/', include('mygpoauth.oauth2.urls', namespace='oauth2')),
 ]
