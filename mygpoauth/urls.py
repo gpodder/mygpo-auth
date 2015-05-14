98aa2b25c63ec5bd6384a9d398a70996799b135e
Stefan Kögl <stefan@skoegl.net>
2015-05-14 11:21:44 +0200

    Make "/" a non-permanent redirect

--- mygpoauth/urls.py
+++ mygpoauth/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     # url(r'^$', 'mygpoauth.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', RedirectView.as_view(url='http://mygpo-auth.rtfd.org/'),
+    url(r'^$', RedirectView.as_view(url='http://mygpo-auth.rtfd.org/',
+                                    permanent=False),
         name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth2/', include('mygpoauth.oauth2.urls', namespace='oauth2')),
