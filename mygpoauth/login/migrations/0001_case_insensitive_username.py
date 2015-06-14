cc1c0d386b2e657c9f1f80a0e1ac1a4375df377b
Stefan Kögl <stefan@skoegl.net>
2015-06-14 16:21:00 +0200

    Add case-insensitive unique index for username

---
+++ mygpoauth/login/migrations/0001_case_insensitive_username.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    """ Create a unique case-insensitive index on the username column """
+
+    dependencies = [
+        ('auth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            'CREATE UNIQUE INDEX user_case_insensitive_unique '
+            'ON auth_user ((lower(username)));',
+            'DROP INDEX user_case_insensitive_unique',
+        ),
+    ]
