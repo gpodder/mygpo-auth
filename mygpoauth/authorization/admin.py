d831ed5bcba891dbe7685f69c34e062ccc782ba9
Stefan Kögl <stefan@skoegl.net>
2016-08-21 19:09:41 +0200

    Add test for Authorization admin section

--- mygpoauth/authorization/admin.py
+++ mygpoauth/authorization/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import Authorization
 
 
+def scope_list(app):
+    """
+    >>> scope_list(Authorization(scopes=['a', 'b', 'c']))
+    'a, b, c'
+    """
+    return ', '.join(app.scopes)
+
+
 @admin.register(Authorization)
 class ApplicationAdmin(admin.ModelAdmin):
 
-    def scope_list(self, app):
-        return ', '.join(app.scopes)
-
-    list_display = ['user', 'application', 'scope_list']
+    list_display = ['user', 'application', scope_list]
 
     list_select_related = ['user', 'application']
 
