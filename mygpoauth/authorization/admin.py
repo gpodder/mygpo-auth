9b968e8cf4c5fd8e4bd120255b8eb3c7bc4e6943
Stefan Kögl <stefan@skoegl.net>
2016-08-21 18:33:51 +0200

    Improve Admin for Authorization objects

--- mygpoauth/authorization/admin.py
+++ mygpoauth/authorization/admin.py
@@ -4,4 +4,14 @@ from .models import Authorization
 
 @admin.register(Authorization)
 class ApplicationAdmin(admin.ModelAdmin):
-    pass
+
+    def scope_list(self, app):
+        return ', '.join(app.scopes)
+
+    list_display = ['user', 'application', 'scope_list']
+
+    list_select_related = ['user', 'application']
+
+    readonly_fields = ['user']
+
+    fields = ['user', 'application', 'scopes', 'code']
